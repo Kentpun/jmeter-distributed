b6fe2fefe62b67bcf43587ce391b1a92374216d2
Kent Pun <kenpun@deloitte.com.hk>
2022-04-03 15:39:40 +0800

    Updated scripts

--- Master/startup-script.sh
+++ Master/startup-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 HostIP=$(ip route show | awk '/default/ {print $9}')
 docker pull kentpun/jmeter-distributed:jmeter-master
-docker run -dit --name master --network host -e HostIP=$HostIP -e Xms=256m -e Xmx=512m -e MaxMetaspaceSize=512m -v /opt/Sharedvolume:/opt/Sharedvolume dragoscampean/testrepo:jmetrumaster /bin/bash
+docker run -dit --name master --network host -e HostIP=$HostIP -e Xms=256m -e Xmx=512m -e MaxMetaspaceSize=512m -v /opt/Sharedvolume:/opt/Sharedvolume kentpun/jmeter-distributed:jmeter-master /bin/bash
